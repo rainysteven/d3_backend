d3bb9534f4b6c83563e76e0f46e40cbd24a6151d
rainysteven <13679731319@163.com>
2022-06-01 11:00:06 +0800

    路由修改

--- backend/routing.py
+++ backend/routing.py
@@ -4,5 +4,4 @@ from backend.wsViews import FilesConsumer
 websocket_urlpatterns = [
     path('ws/records/<str:channel_id>/', FilesConsumer.as_asgi()),
     path('ws/sections/<str:channel_id>/', FilesConsumer.as_asgi())
-    # path('ws/<str:username>/',MessagesConsumer) # 如果是传参的路由在连接中获取关键字参数方法：self.scope['url_route']['kwargs']['username']
 ]
